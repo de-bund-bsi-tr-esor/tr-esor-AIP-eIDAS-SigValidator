784a23834d7af05d1396c37ce8cbcf1f4e341e39
Sebastian Wolff <sebastian.wolff@procilon.de>
2020-12-09 18:02:22 +0100

    PRONASV-58 Integration Lizenztext in Quelldateien

--- default-signature-verifier/src/main/java/de/bund/bsi/tresor/xaip/validator/signature/DefaultSignatureVerifier.java
+++ default-signature-verifier/src/main/java/de/bund/bsi/tresor/xaip/validator/signature/DefaultSignatureVerifier.java
@@ -1,8 +1,28 @@
+/*-
+ * Copyright (c) 2020
+ * Federal Office for Information Security (BSI),
+ * Godesberger Allee 185-189,
+ * 53175 Bonn, Germany,
+ * phone: +49 228 99 9582-0,
+ * fax: +49 228 99 9582-5400,
+ * e-mail: bsi@bsi.bund.de
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.bund.bsi.tresor.xaip.validator.signature;
 
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -11,7 +31,6 @@ import java.util.Optional;
 import java.util.Set;
 
 import org.dom4j.Document;
-import org.dom4j.DocumentFactory;
 import org.dom4j.io.SAXReader;
 
 import de.bund.bsi.tr_esor.vr._1.CredentialValidityType;
@@ -36,21 +55,11 @@ import oasis.names.tc.dss._1_0.core.schema.SignatureObject;
 @Getter
 public class DefaultSignatureVerifier implements SignatureVerifier
 {
-    private final String               vendor    = "BSI";
-    private final String               version   = "1.0.0";
+    private final String          vendor  = "BSI";
+    private final String          version = "1.0.0";
     
-    private DefaultVerifierConfig      config;
-    private VerificationClient         client;
-    
-    private static Map<String, String> nsContext = new HashMap<>();
-    static
-    {
-        nsContext.put( "ds", "http://www.w3.org/2000/09/xmldsig#" );
-        nsContext.put( "dss", "urn:oasis:names:tc:dss:1.0:core:schema" );
-        nsContext.put( "esor", "http://www.bsi.bund.de/tr-esor/xaip/1.2" );
-        
-        DocumentFactory.getInstance().setXPathNamespaceURIs( nsContext );
-    }
+    private DefaultVerifierConfig config;
+    private VerificationClient    client;
     
     /**
      * Configuring this module by using the provided property map
--- xaip-validator-dispatcher/src/main/java/de/bund/bsi/tresor/xaip/validator/dispatcher/DispatcherArguments.java
+++ xaip-validator-dispatcher/src/main/java/de/bund/bsi/tresor/xaip/validator/dispatcher/DispatcherArguments.java
@@ -1,3 +1,24 @@
+/*-
+ * Copyright (c) 2020
+ * Federal Office for Information Security (BSI),
+ * Godesberger Allee 185-189,
+ * 53175 Bonn, Germany,
+ * phone: +49 228 99 9582-0,
+ * fax: +49 228 99 9582-5400,
+ * e-mail: bsi@bsi.bund.de
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.bund.bsi.tresor.xaip.validator.dispatcher;
 
 import java.io.InputStream;
