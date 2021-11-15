1b3fc8fc0dc12626d58af0952676baedfe144051
Sebastian Wolff <sebastian.wolff@procilon.de>
2021-11-15 10:02:28 +0100

    added asic-aip converter for basic aip

--- default-syntax-validator/src/test/java/tools/ASiCAIPConverter.java
+++ default-syntax-validator/src/test/java/tools/ASiCAIPConverter.java
@@ -1,4 +1,4 @@
-package converter;
+package tools;
 
 import static java.util.stream.Collectors.toSet;
 
@@ -12,14 +12,18 @@ import java.nio.file.Path;
 import java.security.KeyStore;
 import java.security.KeyStore.PasswordProtection;
 import java.security.KeyStore.PrivateKeyEntry;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 import javax.xml.bind.JAXB;
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 
@@ -31,11 +35,11 @@ import org.etsi.uri._02918.v1_2.ExtensionsListType;
 import org.etsi.uri._02918.v1_2.ObjectFactory;
 import org.etsi.uri._02918.v1_2.SigReferenceType;
 import org.etsi.uri._19512.v1_1.ContainerIDType;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 import de.bund.bsi.tr_esor.xaip.DataObjectType;
-import de.bund.bsi.tr_esor.xaip.DataObjectsSectionType;
 import de.bund.bsi.tr_esor.xaip.PackageHeaderType;
-import de.bund.bsi.tr_esor.xaip.VersionManifestType;
 import de.bund.bsi.tr_esor.xaip.XAIPType;
 import eu.europa.esig.dss.cades.CAdESSignatureParameters;
 import eu.europa.esig.dss.cades.signature.CAdESService;
@@ -53,6 +57,7 @@ import eu.europa.esig.dss.validation.CommonCertificateVerifier;
 /**
  * @author wolffs
  */
+@Disabled
 public class ASiCAIPConverter
 {
     private final byte[] ksBytes;
@@ -66,32 +71,45 @@ public class ASiCAIPConverter
         this.password = Optional.ofNullable( password ).orElse( "" ).toCharArray();
     }
     
-    public static void main( String[] args ) throws Exception
+    @Test
+    public void execute() throws Exception
     {
+        // Configuration
         String pw = "kennwort";
         String entry = "wolffs";
         String ksPath = "/home/wolffs/wolffs-test";
         File outputFile = new File( "/tmp/resultAsic" );
-        File inputXaip = new File( "/home/wolffs/Dokumente/13.xaip" );
+        // File inputXaip = new File( "/home/wolffs/Dokumente/13.xaip" );
+        File inputXaip = new File( "/tmp/sample.xaip" );
+        //////////////////////////////////////////////////////////////////////
         
         System.out.println( "> starting converstion" );
         
         XAIPType xaip = JAXB.unmarshal( inputXaip, XAIPType.class );
         
         ASiCAIPConverter converter = new ASiCAIPConverter( ksPath, pw, entry );
-        converter.convert( xaip, "v0", outputFile );
+        converter.convert( xaip, outputFile );
         
         System.out.println( "> finished conversion" );
     }
     
-    public void convert( XAIPType xaip, String version, File output ) throws Exception
+    /**
+     * Converting a normal xaip into asic formatted aip
+     * 
+     * @param xaip
+     *            the normal xaip
+     * @param output
+     *            the output file
+     * @throws Exception
+     */
+    public void convert( XAIPType xaip, File output ) throws Exception
     {
         FileUtils.deleteQuietly( output );
         output.mkdir();
         
         String aoid = findAOID( xaip );
         createContainerFile( output, xaip, aoid );
-        createMetaInf( output, xaip, aoid, version );
+        createMetaInf( output, xaip, aoid );
         createMimeFile( output, "application/vnd.etsi.asic-e+zip" );
         
         File file = new File( output.getParent(), aoid + ".asic" );
@@ -121,14 +139,31 @@ public class ASiCAIPConverter
         }
     }
     
-    // DONE
+    /**
+     * Creating the xaip file
+     * 
+     * @param asic
+     *            the asic parent dir
+     * @param xaip
+     *            the xaip
+     * @param aoid
+     *            the xaip aoid
+     */
     void createContainerFile( File asic, XAIPType xaip, String aoid )
     {
         File xaipFile = new File( asic, aoid + ".xaip" );
         JAXB.marshal( new de.bund.bsi.tr_esor.xaip.ObjectFactory().createXAIP( xaip ), xaipFile );
     }
     
-    // DONE
+    /**
+     * Creating the mimetype file
+     * 
+     * @param asic
+     *            the asic parent dir
+     * @param mimeType
+     *            the mimetype
+     * @throws IOException
+     */
     void createMimeFile( File asic, String mimeType ) throws IOException
     {
         File mimetypeFile = new File( asic, "mimetype" );
@@ -138,30 +173,92 @@ public class ASiCAIPConverter
         }
     }
     
-    // DONE
-    void createMetaInf( File asic, XAIPType xaip, String aoid, String version ) throws Exception
+    /**
+     * Creating the META-INF directory and it's content
+     * 
+     * @param asic
+     *            the asic parent dir
+     * @param xaip
+     *            the xaip
+     * @param aoid
+     *            the xaip aoid
+     * @throws Exception
+     */
+    void createMetaInf( File asic, XAIPType xaip, String aoid ) throws Exception
     {
-        checkVersion( xaip, version );
-        
         File metaInf = new File( asic, "META-INF" );
         metaInf.mkdir();
         
-        Set<String> oids = Optional.ofNullable( xaip )
-                .map( XAIPType::getDataObjectsSection )
-                .map( DataObjectsSectionType::getDataObject )
-                .orElse( new ArrayList<>() ).stream()
-                .map( DataObjectType::getDataObjectID )
-                .collect( toSet() );
+        Map<String, Set<String>> oidsByVersion = oidsByVersion( xaip );
         
-        File sigFile = new File( metaInf, aoid + ".p7s" );
-        File manifest = createASiCManifest( metaInf, sigFile, aoid, version, oids );
-        
-        createSig( manifest, sigFile );
+        for ( Entry<String, Set<String>> entry : oidsByVersion.entrySet() )
+        {
+            String version = entry.getKey();
+            Set<String> oids = entry.getValue();
+            
+            File sigFile = new File( metaInf, version + ".p7s" );
+            File manifest = createASiCManifest( metaInf, sigFile, aoid, version, oids );
+            
+            createSig( manifest, sigFile );
+        }
     }
     
+    /**
+     * Mapping the oids by their version
+     * 
+     * @param xaip
+     *            the xaip
+     * @return the map
+     */
+    Map<String, Set<String>> oidsByVersion( XAIPType xaip )
+    {
+        Map<String, Set<String>> oidsByVersion = new HashMap<>();
+        
+        Optional.ofNullable( xaip )
+                .map( XAIPType::getPackageHeader )
+                .map( PackageHeaderType::getVersionManifest )
+                .orElseThrow( () -> new IllegalStateException( "missing versions in xaip" ) )
+                .stream()
+                .forEach( manifest -> {
+                    String version = manifest.getVersionID();
+                    Set<String> oids = manifest.getPackageInfoUnit().stream()
+                            .flatMap( info -> {
+                                return Stream.concat(
+                                        info.getProtectedObjectPointer().stream(),
+                                        info.getUnprotectedObjectPointer().stream() )
+                                        .map( JAXBElement::getValue )
+                                        .filter( DataObjectType.class::isInstance )
+                                        .map( DataObjectType.class::cast )
+                                        .map( DataObjectType::getDataObjectID );
+                                
+                            } )
+                            .collect( toSet() );
+                            
+                    oidsByVersion.put( version, oids );
+                } );
+        
+        return oidsByVersion;
+    }
+    
+    /**
+     * Creating the asic manifest
+     * 
+     * @param metaInf
+     *            the parent metaInf directory
+     * @param sigFile
+     *            the sigFile
+     * @param aoid
+     *            the xaip aoid
+     * @param version
+     *            the xaip version
+     * @param oids
+     *            the objectIds for the provided version
+     * @return the created manifest file
+     * @throws JAXBException
+     */
     File createASiCManifest( File metaInf, File sigFile, String aoid, String version, Set<String> oids ) throws JAXBException
     {
-        File manifestFile = new File( metaInf, "ASiCManifest-" + aoid + ".xml" );
+        File manifestFile = new File( metaInf, "ASiCManifest-" + version + ".xml" );
         
         SigReferenceType sigRef = new SigReferenceType();
         sigRef.setURI( "file://META-INF/" + sigFile.getName() );
@@ -198,7 +295,15 @@ public class ASiCAIPConverter
         return manifestFile;
     }
     
-    // DONE
+    /**
+     * Creating the signature file
+     * 
+     * @param manifest
+     *            the parent manifest dir
+     * @param sigFile
+     *            the output file
+     * @throws Exception
+     */
     void createSig( File manifest, File sigFile ) throws Exception
     {
         DSSDocument toSignDocument = new FileDocument( manifest );
@@ -229,25 +334,18 @@ public class ASiCAIPConverter
         signedDocument.writeTo( new FileOutputStream( sigFile ) );
     }
     
-    // DONE
+    /**
+     * Searching and extracting the aoid
+     * 
+     * @param xaip
+     *            the xaip
+     * @return the aoid
+     */
     String findAOID( XAIPType xaip )
     {
         return Optional.ofNullable( xaip )
                 .map( XAIPType::getPackageHeader )
                 .map( PackageHeaderType::getAOID )
                 .orElseThrow( () -> new IllegalStateException( "missing aoid which is not specified for this format" ) );
-    }
-    
-    // DONE
-    void checkVersion( XAIPType xaip, String version )
-    {
-        Optional.ofNullable( xaip )
-                .map( XAIPType::getPackageHeader )
-                .map( PackageHeaderType::getVersionManifest )
-                .orElse( new ArrayList<>() ).stream()
-                .map( VersionManifestType::getVersionID )
-                .filter( version::equals )
-                .findAny()
-                .orElseThrow( () -> new IllegalArgumentException( "version " + version + " does not exist" ) );
     }
 }
